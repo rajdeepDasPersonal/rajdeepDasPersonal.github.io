b972b8b321ac21cff046a48a17cd5d83270bcb4b
A L E X <rajdeepdas.personal@gmail.com>
2023-07-18 20:28:15 +0530

    update

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+// Preloader
+const preloader = document.querySelector(".preloader");
+const preloaderDuration = 500;
+
+const hidePreloader = () => {
+    setTimeout(() => {
+        preloader.classList.add("hide");
+    }, preloaderDuration);
+}
+
+window.addEventListener("load", hidePreloader);
